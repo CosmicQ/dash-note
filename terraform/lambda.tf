fad98f4dee15cdc54d94cfecd02f1620b05dbbc5
Jason Qualkenbush <jason@cosmicegg.net>
2025-04-24 14:38:24 -0400

    Added description and timeout to lambda.tf (3 seconds was too short)

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -7,9 +7,11 @@ data "archive_file" "lambda_zip" {
 resource "aws_lambda_function" "dash_note" {
   filename         = data.archive_file.lambda_zip.output_path
   function_name    = "dash-note"
+  description      = "dash-note creates vertical annotations on CloudWatch dashboards from EventBridge events"
   role             = aws_iam_role.dash_note_lambda_role.arn
   handler          = "dash_note.lambda_handler"
   runtime          = "python3.12"
+  timeout          = 15
   source_code_hash = filebase64sha256(data.archive_file.lambda_zip.output_path)
 }
 
